aba803e056900c81319c5473ca9f096baa413487
Leon <leon.vanveldhuijzen@gmail.com>
2024-10-25 17:01:04 +0200

    works!

--- esma_prospectus_collector.py
+++ esma_prospectus_collector.py
@@ -1,33 +1,56 @@
-from selenium import webdriver
-from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.support.ui import Select, WebDriverWait
-from selenium.webdriver.common.by import By
-from bs4 import BeautifulSoup
 import pandas as pd
 import time
 
-from utils import parse_table_to_df
+from selenium import webdriver
+from selenium.webdriver.support.ui import Select, WebDriverWait
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
+
+from utils import scrape_esma_table, click_next_page
 
 
+WRITE_RESULT_CSV = 'esma_prospectus_metadata.csv'
 ESMA_PROS_URL = 'https://registers.esma.europa.eu/publication/searchRegister?core=esma_registers_priii_documents'
-ESMA_DOC_DOWNLOAD_BASE_URL = 'https://registers.esma.europa.eu/publication/'
 
 
-# Function to click the "next" page if it's active
-def click_next_page(driver):
-    try:
-        # Locate the "next" page element
-        wait_sec = 3
-        next_button = WebDriverWait(driver, wait_sec).until(
-            EC.presence_of_element_located((By.CSS_SELECTOR, "li.next a.active"))
-        )
-        if next_button:
-            next_button.click()
-            time.sleep(2)  # Allow time for the page to load
-            return True
-    except:
-        print("No more active 'next' button found.")
-        return False
+def apply_filters(driver):
+        """Put all your filters for ESMA prospectus Register here"""
+         
+        # select doctype 
+        dropdown_doc_type = Select(driver.find_element(By.NAME, "document_type"))
+        dropdown_doc_type.select_by_value("BPWO") # Base prospectus without Final terms
+        
+        # # select home member state
+        # dropdown_home_member_state = Select(driver.find_element(By.NAME, "home_member_state_code"))
+        # dropdown_home_member_state.select_by_value("NL") # Netherlands 
+
+        #  add language as option 
+        # Locate the dropdown element by its ID
+        extra_filer_dropdown = Select(driver.find_element(By.ID, "ID005"))
+        # Select the "Language" option by visible text
+        extra_filer_dropdown.select_by_visible_text("Language")
+        # select English
+        dropdown_language = Select(driver.find_element(By.NAME, "document_language_code"))
+        dropdown_language.select_by_value("en")  
+
+        time.sleep(1)
+
+        # add language as col in results 
+        add_col_button = driver.find_element(By.CLASS_NAME, "displayColumn")
+        add_col_button.click()
+
+        time.sleep(1)
+
+        # Locate the "Language" checkbox using its ID
+        language_checkbox = driver.find_element(By.ID, "document_languages")
+        # Click the checkbox
+        language_checkbox.click()
+
+        # click update
+        # Locate the "Update" button using its class name
+        update_button = driver.find_element(By.XPATH, "//button[text()='Update']")
+        update_button.click()
+
 
 
 
@@ -40,51 +63,43 @@ def main():
         # Wait for the page to load completely
         time.sleep(2)
 
-        # select doctype 
-        dropdown_doc_type = Select(driver.find_element(By.NAME, "document_type"))
-        dropdown_doc_type.select_by_value("BPWO") # Base prospectus without Final terms
-        # select home member state
-        dropdown_home_member_state = Select(driver.find_element(By.NAME, "home_member_state_code"))
-        dropdown_home_member_state.select_by_value("NL") # Netherlands 
+        # apply filters for ESMA Prospectus Register
+        apply_filters(driver)
+
+        time.sleep(2)
 
         # Click the search button
         search_button = driver.find_element(By.ID, "searchSolrButton")
         search_button.click()
 
-        time.sleep(1)
+        time.sleep(2)
 
         # Select the dropdown element by its id and choose the option "100"
         n_items_dropdown = Select(driver.find_element(By.ID, "tablePageSize"))
         n_items_dropdown.select_by_value("100")
 
-        time.sleep(3)
+        time.sleep(2)
+
+        # scrape table 
+        table_df = scrape_esma_table(driver)
+        table_df_list = [table_df]
 
         # Loop through all pages until "next" is inactive
         while click_next_page(driver):
-            # Add logic here to parse each page's content
-            pass
+            table_df = scrape_esma_table(driver)
+            print(len(table_df))
+            table_df_list.append(table_df)
+        
 
+        # concat all table dfs
+        final_df = pd.concat(table_df_list, ignore_index=True)
 
-        # # Get doc download links
-        # page_source = driver.page_source
-        # soup = BeautifulSoup(page_source, 'html.parser')
-        # # Find all document links in the table with id "T01"
-        # table = soup.find("table", {"id": "T01"})
-
-        # table_df = parse_table_to_df(table)
-        # # creat working download link
-        # table_df['physical_doc_downl_url'] = ESMA_DOC_DOWNLOAD_BASE_URL + table_df['Physical Document'].astype(str)
-
-  
-
-        print(table_df.head())
-        print(table_df['physical_doc_downl_url'].iloc[0])
-        print(table_df.columns)
+        # write result to csv 
+        final_df.to_csv(WRITE_RESULT_CSV, index=False, encoding="utf-8", sep=";")
 
     finally:
         # Close the browser
         driver.quit()
-
 
 
 if __name__ == '__main__':
--- utils.py
+++ utils.py
@@ -1,5 +1,42 @@
 import pandas as pd 
 from bs4 import BeautifulSoup
+import time
+
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
+
+
+
+def click_next_page(driver):
+    """Function to click the "next" page if it's active"""
+    try:
+        # Locate the "next" page element
+        wait_sec = 3
+        next_button = WebDriverWait(driver, wait_sec).until(
+            EC.presence_of_element_located((By.CSS_SELECTOR, "li.next a.active"))
+        )
+        if next_button:
+            next_button.click()
+            time.sleep(2)  # Allow time for the page to load
+            return True
+    except Exception as e:
+        print(f"Error occurred: {e}")
+        print("No more active 'next' button found.")
+        return False
+
+
+def scrape_esma_table(driver):
+    """Scrape table from ESMA prospects register and transform to df""" 
+    ESMA_DOC_DOWNLOAD_BASE_URL = 'https://registers.esma.europa.eu/publication/'
+    page_source = driver.page_source
+    soup = BeautifulSoup(page_source, 'html.parser')
+    # Find all document links in the table with id "T01"
+    table = soup.find("table", {"id": "T01"})
+    table_df = parse_table_to_df(table)
+    # add working download link 
+    table_df['physical_doc_downl_url'] = ESMA_DOC_DOWNLOAD_BASE_URL + table_df['Physical Document'].astype(str)
+    return table_df
 
 
 def parse_table_to_df(table):
